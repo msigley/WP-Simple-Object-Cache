b852392d2963530f6c666e8edf18115b1189df07
Matthew Sigley <matthew.d.sigley@gmail.com>
2020-02-18 14:15:18 -0500

    Added support for Redis

--- wp-simple-object-cache/lib/object-cache-bridge.php
+++ wp-simple-object-cache/lib/object-cache-bridge.php
@@ -7,6 +7,8 @@ if ( isset( $memcached_servers ) ) { // Use Memcached
 	require_once( $object_cache_base_path . '/apc/object-cache.php' );
 } elseif ( function_exists( 'xcache_get' ) ) { // Use XCACHE
 	require_once( $object_cache_base_path . '/xcache/object-cache.php' );
+} elseif ( class_exists( 'Redis' ) ) { // Use Redis
+	require_once( $object_cache_base_path . '/redis/object-cache.php' );
 } elseif ( function_exists( 'wincache_ucache_get' ) ) { // Use WinCache
 	require_once( $object_cache_base_path . '/wincache/object-cache.php' );
 } else { // No Cache
